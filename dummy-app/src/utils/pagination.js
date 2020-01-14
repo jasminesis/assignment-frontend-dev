73a9af9c5da573097c5347d3eddaddf857e0d7c3
jasminesis <jasmine-aips@hotmail.com>
2020-01-14 18:06:15 +0800

    finished pagination

--- dummy-app/src/utils/pagination.js
+++ dummy-app/src/utils/pagination.js
@@ -1,5 +1,20 @@
 export function paginator(currentPage, totalPages) {
-  let pages = [];
-  // your code here, have fun!
-  return pages;
+    let pages = [];
+
+    let firstPage;
+
+    if (currentPage < 3) {
+        firstPage = 1;
+    } else if (totalPages - currentPage < 2) {
+        firstPage = totalPages - 4
+    } else {
+        firstPage = currentPage - 2;
+    }
+    for (let i = firstPage; i <= totalPages; i++) {
+        pages.push(i)
+        if (pages.length == 5) {
+            break;
+        }
+    }
+    return pages;
 }

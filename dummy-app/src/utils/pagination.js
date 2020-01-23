706785072c4375322774e26cdb7441bde15c972c
Jasmine <jasmine-aips@hotmail.com>
2020-01-23 15:24:02 +0800

    added more tests and corrected for less than three

--- dummy-app/src/utils/pagination.js
+++ dummy-app/src/utils/pagination.js
@@ -1,20 +1,20 @@
 export function paginator(currentPage, totalPages) {
-    let pages = [];
+	let pages = [];
 
-    let firstPage;
+	let firstPage;
 
-    if (currentPage < 3) {
-        firstPage = 1;
-    } else if (totalPages - currentPage < 2) {
-        firstPage = totalPages - 4
-    } else {
-        firstPage = currentPage - 2;
-    }
-    for (let i = firstPage; i <= totalPages; i++) {
-        pages.push(i)
-        if (pages.length == 5) {
-            break;
-        }
-    }
-    return pages;
+	if (currentPage <= 3) {
+		firstPage = 1;
+	} else if (totalPages - currentPage < 2) {
+		firstPage = totalPages - 4;
+	} else {
+		firstPage = currentPage - 2;
+	}
+	for (let i = firstPage; i <= totalPages; i++) {
+		pages.push(i);
+		if (pages.length == 5) {
+			break;
+		}
+	}
+	return pages;
 }
